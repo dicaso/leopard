1cd822dab2703f39898afab5749b4f8faabef793
Christophe Van Neste <christophe.vanneste@ugent.be>
2020-09-03 12:45:16 +0200

    version+

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = 'leopard',
-      version = '0.2.1',
+      version = '0.2.2',
       description = 'Fast lab reporting python package',
       url = 'https://github.com/beukueb/leopard',
       author = 'Christophe Van Neste',
